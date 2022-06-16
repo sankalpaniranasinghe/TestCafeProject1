3f6627aa5699241ddb5ffbedbbaa7ee43fda4f96
sankalpaniranasinghe <55609164+sankalpaniranasinghe@users.noreply.github.com>
2022-06-16 08:05:54 +0530

    Update LoginPOM.js

--- LoginPOM.js
+++ LoginPOM.js
@@ -3,7 +3,7 @@ import { Selector } from "testcafe";
 const userName= Selector("#txtUserName");
 const password = Selector("#txtPassword");
 const submtButton = Selector("#btnLogin");
-const verify = Selector('.welcome-text');
+const verify = Selector('.left.welcome-text');
 
 fixture `MyFixture2`
     .page `https://portal.qa.kidsxap.com.au/`;
@@ -15,10 +15,9 @@ test
     .typeText(userName,"pk@xaptech.com.au")
     .typeText(password ,"Abcd1234$")
     .click(submtButton)
-    //.expect((x).exists).ok()
+   
    .expect((verify).withExactText('Hello,Piyush').exists).ok();
-   //.expect(verify.value).eql('','Hello,Piyush');
-
+  
     
 });
 
